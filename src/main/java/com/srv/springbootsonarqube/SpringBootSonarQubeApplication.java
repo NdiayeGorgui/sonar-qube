710e50ab844055713e91c145e9fe73c251d41dd3
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-08-15 16:20:03 -0400

    tree

--- src/main/java/com/srv/springbootsonarqube/SpringBootSonarQubeApplication.java
+++ src/main/java/com/srv/springbootsonarqube/SpringBootSonarQubeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringBootSonarQubeApplication {
-String s="";
+public String s="";
 
 	@GetMapping("/test")
 	public String getMessage() {
